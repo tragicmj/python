c6a98c524284cabc65167628c03cae11212ca6da
javed432 <javed.akhtar432@gmail.com>
2020-03-03 23:23:00 +0530

    pythonCourse,py created using which we can add,delete and display courses

---
+++ lco/test.py
@@ -0,0 +1,18 @@
+balance = 4*5*(6/2)
+print(balance)
+print('HELLO WORLD!')
+# array
+iceteas = ['lemon', 'peach', 'mint']
+print(iceteas)
+print(iceteas[2])
+# for loop
+for superman in iceteas:
+    print(superman)
+# function
+
+
+def make_tea():
+    print("I can make ice tea")
+
+
+make_tea()
